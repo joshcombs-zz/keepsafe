dad6e9703b7606812618c90c7afe552e5d920e9b
joshcombs <josh.combs@me.com>
2013-08-19 10:39:38 -0400

    updated errors

--- contact.php
+++ contact.php
@@ -15,7 +15,33 @@ $title = "KeepSafe Security &middot; Contact Us";
 <div class="row-fluid">
 	<div class="span8">
 		<div class="well">
-			<iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=132+Modular+Drive,+Statesville,+NC&amp;aq=0&amp;oq=132+modular+dri&amp;sll=37.6,-95.665&amp;sspn=38.406429,92.988281&amp;ie=UTF8&amp;hq=&amp;hnear=132+Modular+Dr,+Statesville,+North+Carolina+28625&amp;t=m&amp;ll=35.782658,-80.973701&amp;spn=0.02437,0.008583&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
+
+
+			<div class="header">
+	<h3>Send us an Email!</h3>
+
+<?php  
+// check for a successful form post  
+if (isset($_GET['s'])) echo "<div class=\"alert alert-success\">".$_GET['s']."</div>";  
+// check for a form error  
+elseif (isset($_GET['e'])) echo "<div class=\"alert alert-error\">".$_GET['e']."</div>"; 
+?>  
+
+</div>
+			<form method="POST" action="contact-form-submission.php">
+				<div class="controls controls-row">
+					<input id="name" name="first_name" type="text" class="span6" placeholder="Name"> 
+					<input id="email" name="contact_email" type="email" class="span6" placeholder="Email address">
+				</div>
+				<div class="controls">
+					<textarea id="message" name="contact_message" class="span12" placeholder="Your Message" rows="5"></textarea>
+				</div>
+
+				<div class="controls">
+					<input type="hidden" name="save" value="contact">
+					<button id="contact-submit" type="submit" class="btn btn-primary input-medium pull-right">Send</button>
+				</div>
+			</form>
 		</div>
 	</div>
 
@@ -36,36 +62,9 @@ $title = "KeepSafe Security &middot; Contact Us";
 	</div>
 </div>
 <div class="row-fluid">
-	<div class="span8">
+	<div class="span12">
 		<div class="well">
-			<div class="header">
-	<h3>Send us an Email!</h3>
-
-<?php  
-  
-        // check for a successful form post  
-        if (isset($_GET['s'])) echo "<div class=\"alert alert-success\">".$_GET['s']."</div>";  
-  
-        // check for a form error  
-        elseif (isset($_GET['e'])) echo "<div class=\"alert alert-error\">".$_GET['e']."</div>";  
-  
-?>  
-
-</div>
-			<form method="POST" action="contact-form-submission.php">
-				<div class="controls controls-row">
-					<input id="name" name="first_name" type="text" class="span6" placeholder="Name"> 
-					<input id="email" name="contact_email" type="email" class="span6" placeholder="Email address">
-				</div>
-				<div class="controls">
-					<textarea id="message" name="contact_message" class="span12" placeholder="Your Message" rows="5"></textarea>
-				</div>
-
-				<div class="controls">
-					<input type="hidden" name="save" value="contact">
-					<button id="contact-submit" type="submit" class="btn btn-primary input-medium pull-right">Send</button>
-				</div>
-			</form>
+			<iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=132+Modular+Drive,+Statesville,+NC&amp;aq=0&amp;oq=132+modular+dri&amp;sll=37.6,-95.665&amp;sspn=38.406429,92.988281&amp;ie=UTF8&amp;hq=&amp;hnear=132+Modular+Dr,+Statesville,+North+Carolina+28625&amp;t=m&amp;ll=35.782658,-80.973701&amp;spn=0.02437,0.008583&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
 		</div>
 	</div>
 </div>
